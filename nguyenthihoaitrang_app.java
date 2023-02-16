4a5c762c3c157ced5719d95b4607e5fd94b2daee
Lynotcute <119957790+Lynotcute@users.noreply.github.com>
2023-02-16 23:24:10 +0700

    Create nguyenthihoaitrang_app.java

---
+++ nguyenthihoaitrang_app.java
@@ -0,0 +1,10 @@
+package nguyenthihoaitrang_project;
+
+public class nguyenthihoaitrang_app 
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Nguyen Thi Hoai Trang hoc Java");
+	}
+
+}
